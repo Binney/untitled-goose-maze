4be9a226b22b744e609c01b058b4d9ca7b09f961
Catalin Ursachi <Catalin.Ursachi@softwire.com>
2019-10-17 01:21:54 +0100

    HONK-418 - Add validator

---
+++ zorkify/validator.js
@@ -0,0 +1,33 @@
+function validateLocations(locations) {
+  const locationHash = {};
+  locations.forEach(location => {
+    locationHash[location.x] |= {};
+    return locationHash[location.x][location.y] = location;
+  });
+
+  for (let i in locations) {
+    const location = locations[i];
+    const x = location.x;
+    const y = location.y;
+
+    if (location.north && !(locationHash[x] && locationHash[x][y - 1] && locationHash[x][y - 1].south)) {
+      return `Invalid North for ${x},${y}`;
+    }
+
+    if (location.south && !(locationHash[x] && locationHash[x][y + 1] && locationHash[x][y + 1].north)) {
+      return `Invalid South for ${x},${y}`;
+    }
+
+    if (location.west && !(locationHash[x - 1] && locationHash[x - 1][y] && locationHash[x - 1][y].east)) {
+      return `Invalid West for ${x},${y}`;
+    }
+
+    if (location.east && !(locationHash[x + 1] && locationHash[x + 1][y] && locationHash[x + 1][y].west)) {
+      return `Invalid East for ${x},${y}`;
+    }
+  }
+
+  return "Maze is valid";
+}
+
+module.exports = validateLocations;
--- zorkify/zorkify.js
+++ zorkify/zorkify.js
@@ -1,10 +1,13 @@
 const { Location, Intro } = require("./code_blocks");
+const validateMaze = require("./validator");
 yaml = require('js-yaml');
 fs   = require('fs');
 
 const mazeDoc = yaml.safeLoad(fs.readFileSync('./maze.yaml', 'utf8'));
 
 const locations = mazeDoc.locations.map(locationDoc => new Location(locationDoc));
+
+console.log(validateMaze(locations));
 
 const intro = new Intro(mazeDoc.intro, 13);
 
